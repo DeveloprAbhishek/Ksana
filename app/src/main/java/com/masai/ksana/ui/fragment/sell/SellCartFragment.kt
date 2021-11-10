edc6360636ea3685f1f15b0bf02c860c4ac744a7
MuraliKrishnaSundara <murali.sundara@ssipmt.com>
2021-11-10 20:33:54 +0530

    Sell in progress

--- app/src/main/java/com/masai/ksana/ui/fragment/sell/SellCartFragment.kt
+++ app/src/main/java/com/masai/ksana/ui/fragment/sell/SellCartFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.masai.ksana.R
+import kotlinx.android.synthetic.main.fragment_sell_cart.*
+import kotlinx.android.synthetic.main.fragment_sell_home.*
 
 class SellCartFragment : Fragment() {
 
@@ -15,6 +18,20 @@ class SellCartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sell_cart, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnAddPersonFab.setOnClickListener {
+            val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
+            ft.replace(
+                R.id.framelayout_container,
+                AddNewProductFragment(),
+                "Add New Product Fragment"
+            )
+            ft.addToBackStack(null)
+            ft.commit()
+        }
     }
 
 }
