43c4fe6860977d91bed46b5a83d48e55d8610c5c
MuraliKrishnaSundara <murali.sundara@ssipmt.com>
2021-11-11 20:32:39 +0530

    Dummy Login

--- app/src/main/java/com/masai/ksana/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/masai/ksana/ui/activity/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.masai.ksana.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.masai.ksana.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnSell.setOnClickListener {
+            val i = Intent(this@LoginActivity, SellActivity::class.java)
+            startActivity(i)
+        }
+        btnBuy.setOnClickListener {
+            val i = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(i)
+        }
     }
 }
