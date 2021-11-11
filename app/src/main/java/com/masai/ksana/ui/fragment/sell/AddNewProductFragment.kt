b93a5a35876c124526741283d2828c5e64814d91
MuraliKrishnaSundara <murali.sundara@ssipmt.com>
2021-11-11 16:18:43 +0530

    Firebase adding done

--- app/src/main/java/com/masai/ksana/ui/fragment/sell/AddNewProductFragment.kt
+++ app/src/main/java/com/masai/ksana/ui/fragment/sell/AddNewProductFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
 import com.masai.ksana.R
+import kotlinx.android.synthetic.main.fragment_add_new_product.*
 
 class AddNewProductFragment : Fragment() {
 
@@ -17,4 +20,70 @@ class AddNewProductFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_new_product, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnProceed.setOnClickListener {
+            saveProduct()
+        }
+    }
+
+    private fun saveProduct() {
+        val productName = etEnterProductName.text.toString().trim()
+        if (productName.isEmpty()) {
+            etEnterProductName.error = "Please Enter Product Name"
+            return
+        }
+        val materialGrade = etEnterMaterialGrade.text.toString().trim()
+        if (materialGrade.isEmpty()) {
+            etEnterMaterialGrade.error = "Please Enter Product Name"
+            return
+        }
+        val unitLength = etEnterUnitLength.text.toString().trim()
+        if (unitLength.isEmpty()) {
+            etEnterUnitLength.error = "Please Enter Product Name"
+            return
+        }
+        val diameter = etEnterDiameter.text.toString().trim()
+        if (diameter.isEmpty()) {
+            etEnterDiameter.error = "Please Enter Product Name"
+            return
+        }
+        val quantity = etEnterQuantity.text.toString().trim()
+        if (quantity.isEmpty()) {
+            etEnterQuantity.error = "Please Enter Product Name"
+            return
+        }
+
+        val reference = FirebaseDatabase.getInstance().getReference("products")
+        val productId = reference.push().key
+
+        val product =
+            productId?.let {
+                SellProductList(
+                    it,
+                    productName,
+                    materialGrade,
+                    unitLength,
+                    diameter,
+                    quantity
+                )
+            }
+        if (productId != null) {
+            reference.child(productId).setValue(product).addOnCompleteListener {
+                Toast.makeText(context, "Product Added Successfully", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
+
+
+
+/*
+{
+  "rules": {
+    ".read": "now < 1639161000000",  // 2021-12-11
+    ".write": "now < 1639161000000",  // 2021-12-11
+  }
+}
+ */
---
+++ app/src/main/java/com/masai/ksana/ui/fragment/sell/SellProductList.kt
@@ -0,0 +1,10 @@
+package com.masai.ksana.ui.fragment.sell
+
+class SellProductList(
+    val id: String,
+    val productName: String,
+    val materialGrade: String,
+    val unitLength: String,
+    val diameter: String,
+    val quantity: String
+)
