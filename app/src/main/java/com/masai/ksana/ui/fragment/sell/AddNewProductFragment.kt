a6d32fde685b43347853c2cb1f4b38a484482e03
MuraliKrishnaSundara <murali.sundara@ssipmt.com>
2021-11-12 16:20:48 +0530

    photo uploading working

--- app/src/main/java/com/masai/ksana/ui/fragment/sell/AddNewProductFragment.kt
+++ app/src/main/java/com/masai/ksana/ui/fragment/sell/AddNewProductFragment.kt
@@ -1,7 +1,9 @@
 package com.masai.ksana.ui.fragment.sell
 
 import android.app.Activity
+import android.app.Application
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
@@ -12,22 +14,17 @@ import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.masai.ksana.R
-import com.masai.ksana.ui.activity.SellActivity
-import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.fragment_add_new_product.*
 import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.navigation.ActivityNavigatorExtras
-import java.lang.NullPointerException
-import android.content.ContentValues
-import android.graphics.Bitmap
 import android.os.Environment
 import androidx.core.content.FileProvider
-import kotlinx.android.synthetic.main.activity_home.mainActivity
-import kotlinx.android.synthetic.main.activity_sell.*
-import okhttp3.internal.notify
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.ByteArrayOutputStream
 import java.io.File
-import java.net.URI
+import android.provider.MediaStore.Images
+import androidx.core.net.toUri
 
 
 class AddNewProductFragment : Fragment() {
@@ -35,8 +32,12 @@ class AddNewProductFragment : Fragment() {
     private lateinit var database: FirebaseDatabase
     private lateinit var reference: DatabaseReference
 
-    private lateinit var fileUri: String
+
+    private lateinit var storageReference: StorageReference
+    private lateinit var imageUri: Uri
+
     private lateinit var photoFile: File
+    private lateinit var productId: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,8 +55,19 @@ class AddNewProductFragment : Fragment() {
         }
 
         btnUpload.setOnClickListener {
+
+
+            /* val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+             //imageUri = Uri.parse(photoFile.absolutePath)
+             if (this.activity?.let { it1 -> takePictureIntent.resolveActivity(it1.packageManager) } != null)
+                 startActivityForResult(takePictureIntent, Companion.REQUEST_CODE)
+             else
+                 Toast.makeText(context, "Unable to open camera", Toast.LENGTH_SHORT).show()*/
+
+
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             photoFile = getPhotoFile(FILE_NAME)
+            //imageUri = Uri.parse(photoFile.absolutePath)
 
             val fileProvider =
                 context?.let { it1 ->
@@ -85,11 +97,29 @@ class AddNewProductFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        /* if (requestCode == Companion.REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+             val takenImage = data?.extras?.get("data") as Bitmap
+             btnUpload.setImageBitmap(takenImage)
+             /*val bytes = ByteArrayOutputStream()
+             takenImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+             val path: String =
+                 Images.Media.insertImage(context?.getContentResolver(), takenImage, "Title", null)
+             imageUri = Uri.parse(path)*/
+         } else
+             super.onActivityResult(requestCode, resultCode, data)
+     }*/
+
         if (requestCode == Companion.REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+
+            //imageUri = Uri.parse(photoFile.absolutePath)
+
             btnUpload.setImageBitmap(takenImage)
         } else
             super.onActivityResult(requestCode, resultCode, data)
+
+        imageUri = photoFile.toUri()
+
     }
 
 
@@ -138,24 +168,66 @@ class AddNewProductFragment : Fragment() {
             }
         if (productId != null) {
             reference.child(productId).setValue(product).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    uploadProductImage(productId)
+                } else {
+                    Toast.makeText(context, "Failed to upload Product", Toast.LENGTH_SHORT)
+                        .show()
+                }
                 Toast.makeText(context, "Product Added Successfully", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
+    private fun uploadProductImage(id: String) {
+        //imageUri = Uri.parse(btnUpload.)
+        storageReference = FirebaseStorage.getInstance().getReference("Products/" + id)
+        storageReference.putFile(imageUri).addOnSuccessListener {
+            Toast.makeText(context, "Image Added Successfully", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(context, "Failed to upload Product", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     companion object {
         private const val REQUEST_CODE = 42
-        private const val FILE_NAME = "photo.jpg"
+        private const val FILE_NAME = "photo"
     }
 
 }
 
 
 /*
+
+storage rule
+rules_version = '2';
+service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read, write: if request.auth != null;
+    }
+  }
+}
+
+
+realtime rule
 {
   "rules": {
     ".read": "now < 1639161000000",  // 2021-12-11
     ".write": "now < 1639161000000",  // 2021-12-11
   }
 }
+
+
+
+
+<provider
+            android:name="androidx.core.content.FileProvider"
+            android:authorities="com.masai.ksana.fileprovider"
+            android:exported="false"
+            android:grantUriPermissions="true">
+            <meta-data
+                android:name="android.support.FILE_PROVIDER_PATHS"
+                android:resource="@xml/fileprovider" />
+        </provider>
  */
