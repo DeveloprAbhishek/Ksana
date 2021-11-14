d0c878295120538245e23999c293bfd737ffc26f
MuraliKrishnaSundara <murali.sundara@ssipmt.com>
2021-11-14 12:07:30 +0530

    Menu Screen Logic Done

--- app/src/main/java/com/masai/ksana/ui/fragment/buy/MenuFragment.kt
+++ app/src/main/java/com/masai/ksana/ui/fragment/buy/MenuFragment.kt
@@ -1,11 +1,15 @@
 package com.masai.ksana.ui.fragment.buy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.masai.ksana.R
+import com.masai.ksana.ui.activity.SellActivity
+import kotlinx.android.synthetic.main.fragment_menu.*
 
 class MenuFragment : Fragment() {
 
@@ -15,6 +19,33 @@ class MenuFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnGoToSell.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, SellActivity::class.java)
+                it.startActivity(intent)
+            }
+        }
+
+        ivProfile.setOnClickListener {
+            val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
+            ft.replace(
+                R.id.framelayout_container,
+                ProfileFragment(),
+                "Profile Fragment"
+            )
+            ft.addToBackStack(null)
+            ft.commit()
+        }
+
+        ivCart.setOnClickListener {
+
+        }
+
     }
 
 }
--- app/src/main/java/com/masai/ksana/ui/fragment/sell/SellMenuFragment.kt
+++ app/src/main/java/com/masai/ksana/ui/fragment/sell/SellMenuFragment.kt
@@ -1,11 +1,15 @@
 package com.masai.ksana.ui.fragment.sell
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
 import com.masai.ksana.R
+import com.masai.ksana.ui.activity.HomeActivity
+import kotlinx.android.synthetic.main.fragment_sell_menu.*
 
 class SellMenuFragment : Fragment() {
 
@@ -15,6 +19,40 @@ class SellMenuFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sell_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnGoToBuy.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, HomeActivity::class.java)
+                it.startActivity(intent)
+            }
+        }
+
+        ivProfile.setOnClickListener {
+            val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
+            ft.replace(
+                R.id.framelayout_container,
+                SellProfileFragment(),
+                "Sell Profile Fragment"
+            )
+            ft.addToBackStack(null)
+            ft.commit()
+        }
+
+        ivInventory.setOnClickListener {
+            val ft: FragmentTransaction = parentFragmentManager.beginTransaction()
+            ft.replace(
+                R.id.framelayout_container,
+                SellCartFragment(),
+                "Sell Cart Fragment"
+            )
+            ft.addToBackStack(null)
+            ft.commit()
+        }
+
     }
 
 }
